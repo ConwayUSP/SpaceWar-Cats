d3689fe670f1f2d438dd1b1a51dc4080afa13659
Filipi <filipi.grios@gmail.com>
2026-08-11 20:41:54 -0300

    feat(spawner): can set max number of enemies on screen

--- modules/entities/spawner.lua
+++ modules/entities/spawner.lua
@@ -7,15 +7,17 @@ Spawner.__index = Spawner
 Spawner.type = "Spawner"
 
 
-function Spawner.new(enemyFunc, interval, startDelay, duration)
+function Spawner.new(enemyName, enemyFunc, interval, startDelay, duration, maxEnemiesOnScreen)
     local spawner = setmetatable({}, Spawner)
 
+    spawner.enemyName = enemyName
     spawner.enemyFunc = enemyFunc
     spawner.intervalInit = interval
     spawner.interval = interval
     spawner.timer = -(startDelay or 0) + interval
     spawner.duration = duration or 60
     spawner.totalTime = 0
+    spawner.maxEnemiesOnScreen = maxEnemiesOnScreen or math.huge
 
     return spawner
 end
@@ -24,7 +26,7 @@ function Spawner:update(dt)
     self.timer = self.timer + dt
     self.totalTime = self.totalTime + dt
     if self.timer >= self.interval then
-        if self.totalTime < self.duration then
+        if self.totalTime < self.duration and enemyManager:getCounter(self.enemyName) < self.maxEnemiesOnScreen then
             self.enemyFunc()
         end
         self.interval = self.intervalInit + math.random(-0.9 * self.interval, 0.9 * self.interval)

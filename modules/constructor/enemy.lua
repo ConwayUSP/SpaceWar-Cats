23a603bdc129453f0bea6470de96e4ff4633dced
Filipi <filipi.grios@gmail.com>
2026-08-11 21:13:06 -0300

    Change drone enemy movement

--- modules/constructor/enemy.lua
+++ modules/constructor/enemy.lua
@@ -31,7 +31,7 @@ function newShooterEnemy(x, y)
 
     local function move(self, dt)
         local error = (VIRTUAL_WIDTH - 50) - self.body:getX()
-        local vx = dt * -1500 * math.random(0.8, 1.2)
+        local vx = dt * -2500 * (math.cos(self.timer * math.pi * 0.2) ^ 4)
         local vy = 1000 * math.cos(self.timer * math.pi * 0.2) * dt
         self.body:setLinearVelocity(vx, vy)
     end

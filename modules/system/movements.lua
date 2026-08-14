96be32457a33a780395794896e1d46d131f64167
Filipi <filipi.grios@gmail.com>
2026-08-14 19:50:13 -0300

    fix usage of delta timee to prevent framerate dependent gameplay

--- modules/system/movements.lua
+++ modules/system/movements.lua
@@ -1,20 +1,20 @@
 function moveLeft(e, dt)
-  e.body:setLinearVelocity(-e.speed * dt, 0)
+  e.body:setLinearVelocity(-e.speed, 0)
 end
 
 function moveRight(e, dt)
-  e.body:setLinearVelocity(e.speed * dt, 0)
+  e.body:setLinearVelocity(e.speed, 0)
 end
 
 function moveDirection(e, dt)
-  local vx = e.speed * math.cos(e.dir) * dt
-  local vy = e.speed * math.sin(e.dir) * dt
+  local vx = e.speed * math.cos(e.dir)
+  local vy = e.speed * math.sin(e.dir)
   e.body:setLinearVelocity(vx, vy)
 end
 
 function moveCircular(e, dt)
   e.dir = e.dir + e.turnSpeed * dt
-  local vx = e.speed * math.cos(e.dir) * dt - e.speed * dt
-  local vy = e.speed * math.sin(e.dir) * dt
+  local vx = e.speed * math.cos(e.dir) - e.speed
+  local vy = e.speed * math.sin(e.dir)
   e.body:setLinearVelocity(vx, vy)
 end
